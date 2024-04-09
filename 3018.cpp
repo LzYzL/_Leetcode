b777d5341094d391ff5b5a5dfdb5d069792592b6
lzyzl <lzyzlzyzl@outlook.com>
2024-04-09 23:55:01 +0100

    asdf

--- 3018.cpp
+++ 3018.cpp
@@ -1,30 +1,39 @@
-class Solution {
+//遍历连通的所有边可得最小值，故以并查集维护答案
+class Solution
+{
 public:
-    int fa[100006],w[100006];
-    int getf(int x) {
-        return fa[x]==x?x:fa[x]=getf(fa[x]);
+    int fa[100006], w[100006];
+    int getf(int x)
+    {
+        return fa[x] == x ? x : fa[x] = getf(fa[x]);
     }
     void pre(int n)
     {
-        for (int i=0;i<n;++i) {
-            fa[i]=i;
-            w[i]=-1;
+        for (int i = 0; i < n; ++i)
+        {
+            fa[i] = i;
+            w[i] = -1;
         }
     }
-    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        //memset(fa,0,sizeof(fa));
+    vector<int> minimumCost(int n, vector<vector<int>> &edges, vector<vector<int>> &query)
+    {
+        // memset(fa,0,sizeof(fa));
         pre(n);
-        for (vector<int> edg:edges) {
-            int x=getf(edg[0]),y=getf(edg[1]);
-            fa[y]=x;
-            w[x]=w[x]&w[y]&edg[2];
+        for (vector<int> edg : edges)
+        {
+            int x = getf(edg[0]), y = getf(edg[1]);
+            fa[y] = x;
+            w[x] = w[x] & w[y] & edg[2];
         }
 
         vector<int> ans;
-        for (vector<int> q:query) {
-            int x=getf(q[0]),y=getf(q[1]);
-            if (x!=y) ans.push_back(-1);
-            else {
+        for (vector<int> q : query)
+        {
+            int x = getf(q[0]), y = getf(q[1]);
+            if (x != y)
+                ans.push_back(-1);
+            else
+            {
                 ans.push_back(w[x]);
             }
         }
